1bfce3e16145277741b06aedff537c4f94574459
Dev Patel <95192549+DevP25@users.noreply.github.com>
2023-11-14 22:50:22 -0500

    Non-Authenticated Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="Title">
+        <div className="header1">
+          <h3> First IM Fantasy App </h3>
+        </div>
+        <div className="header2">
+          <h1> GET YOUR FIRST <br></br> DEPOSIT MATCHED <br></br> UP TO <b className="Green">$1000</b> </h1>
+        </div>
+        <div className="headerImg">
+          <img src="https://static.vecteezy.com/system/resources/previews/024/043/918/original/money-bag-clipart-transparent-background-free-png.png" alt="money bag"></img>
+        </div>
+        <div className="headerImg2">
+          <img src="https://static.vecteezy.com/system/resources/previews/024/043/918/original/money-bag-clipart-transparent-background-free-png.png" alt="money bag"></img>
+        </div>
+      </section>
+
+      <section className="Images">
+        <img src="https://www.wwlp.com/wp-content/uploads/sites/26/2022/07/Dono-1-copy-1.jpg?strip=1" alt="Ultimate Frisbee"></img>
+        <img src="https://www.nsu.edu/NSU/media/Photos/2017/06/Intramural%20Football/TMC-2479-04-Intramural-Football-046.jpg" alt="Football"></img>
+        <img src="https://www.keene.edu/ksc/assets/files/38364/ww_intramuralbasketball.standard-landscape.1028x579.jpg?20200323150352" alt="Basketball"></img>
+      </section>
     </div>
   );
 }
