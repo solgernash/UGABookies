dcaa842a34a5e7bbe5382dd0906cfc3ff0a5b6df
Dev Patel <95192549+DevP25@users.noreply.github.com>
2023-11-28 16:50:55 -0500

    AuthPage updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import AuthPage from './Components/AuthPage';
-import BetPlacer from './Components/BetPlacer';
+import BasketballBet from './Components/BasketballBet';
+import FootballBet from './Components/FootballBet';
+import UltimateFrBet from './Components/UltimateFrBet';
 
 function App() {
   return (
@@ -11,11 +13,13 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route path='/Authenticated' element={<AuthPage />} />
-          <Route path='/BetForm' element={<BetPlacer />} />
+          <Route path='/Basketball' element={<BasketballBet />} />
+          <Route path='/Football' element={<FootballBet />} />
+          <Route path='/Ultimate_Frisbee' element={<UltimateFrBet />} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-export default App; 
+export default App;
