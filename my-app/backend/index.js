000b62d73344dc4025c0197aaadbe78bfdeb8bc1
Dev Patel <95192549+DevP25@users.noreply.github.com>
2023-12-03 16:48:47 -0500

    Working server connect

---
+++ my-app/backend/index.js
@@ -0,0 +1,71 @@
+// Code for mongoose config in backend
+// Filename - backend/index.js
+
+// To connect with your mongoDB database
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const conn_str = 'mongodb://localhost:27017/';
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str)
+.then(() => {
+    app.listen(port);
+    console.log('MongoDB Connection Suceeded...');
+})
+.catch(err => {
+    console.log('err');
+});
+
+// Schema for users of app
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+const User = mongoose.model('users', UserSchema);
+User.createIndexes();
+
+// For backend and express
+const express = require('express');
+const app = express();
+const cors = require("cors");
+console.log("App listen at port 5000");
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, resp) => {
+
+	resp.send("App is Working");
+	// You can check backend is working or not by 
+	// entering http://loacalhost:5000
+	
+	// If you see App is working means
+	// backend working properly
+});
+
+app.post("/register", async (req, resp) => {
+	try {
+		const user = new User(req.body);
+		let result = await user.save();
+		result = result.toObject();
+		if (result) {
+			delete result.password;
+			resp.send(req.body);
+			console.log(result);
+		} else {
+			console.log("User already register");
+		}
+
+	} catch (e) {
+		resp.send("Something Went Wrong");
+	}
+});
+// app.listen(5000);
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 import Homepage from './Components/Homepage';
 import AuthPage from './Components/AuthPage';
 import BasketballBet from './Components/BasketballBet';
@@ -10,26 +9,41 @@ import UltimateFrBet from './Components/UltimateFrBet';
 import Instructions from './Components/instructions';
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    axios.get("/api/items")
-      .then(response => setItems(response.data))
-      .catch(error => console.error(error));
-  }, []);
+  const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const handleOnSubmit = async (e) => {
+		e.preventDefault();
+		let result = await fetch(
+		'http://localhost:5000/register', {
+			method: "post",
+			body: JSON.stringify({ name, email }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		result = await result.json();
+		console.warn(result);
+		if (result) {
+			alert("Data saved succesfully");
+			setEmail("");
+			setName("");
+		}
+	}
 
   return (
     <Router>
       <div>
-      <h1>Items</h1>
-      <ul>
-        {items.map(item => (
-          <li key={item._id}>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-          </li>
-        ))}
-      </ul>
+      
+        <h1>This is React WebApp </h1>
+        <form action="">
+          <input type="text" placeholder="name"
+          value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="email" placeholder="email"
+          value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button type="submit"
+          onClick={handleOnSubmit}>submit</button>
+        </form>
+
         <Routes>
           <Route exact path='/' element={<Homepage />} />
           <Route path='/Authenticated' element={<AuthPage />} />
